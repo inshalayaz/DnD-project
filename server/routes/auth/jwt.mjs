a99dfe8744af7753e1b8dee4904dccacf9be9c5e
inshal ayaz <inshal.ayaz14@gmail.com>
2021-10-21 23:15:14 +0500

    jwt verify function

--- server/routes/auth/jwt.mjs
+++ server/routes/auth/jwt.mjs
@@ -9,3 +9,21 @@ export const createToken = (user) => {
     }
   );
 };
+
+export const verifyToken = (req, res, next) => {
+  const token = req.headers.token;
+
+  if (token) {
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+      if (err) {
+        res.status(403).json("Invalid Token");
+      } else {
+        req.user = user;
+      }
+    });
+  } else {
+    res.status(401).json("Auth TOken Required");
+  }
+
+  next();
+};
